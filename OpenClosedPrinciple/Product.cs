4624039650d8a24da5f945ed3ec8c30c19edc642
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-06-09 17:55:20 +0330

    Added Product class and defined several new product types.

---
+++ OpenClosedPrinciple/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int ProductType { get; set; }
+        public double GetDiscount()
+        {
+            if (ProductType == 1)
+            {
+                return (Price / 100) * 5;
+            }
+
+            if (ProductType == 2)
+            {
+                return (Price / 100) * 10;
+            }
+            if (ProductType == 3)
+            {
+                return (Price / 100) * 15;
+            }
+
+            return 0;
+        }
+    }
