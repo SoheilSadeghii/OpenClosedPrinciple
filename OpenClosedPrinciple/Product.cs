a0aaf6b1021b647d331037e6f360f9e60af32863
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-06-09 18:01:47 +0330

    Implemented Open/Closed Principle by closing the class to direct modification.

--- OpenClosedPrinciple/Product.cs
+++ OpenClosedPrinciple/Product.cs
@@ -11,23 +11,29 @@ namespace OpenClosedPrinciple
         public string Name { get; set; }
         public int Price { get; set; }
         public int ProductType { get; set; }
-        public double GetDiscount()
+
+        public virtual double GetDiscount()
         {
-            if (ProductType == 1)
-            {
-                return (Price / 100) * 5;
-            }
-
-            if (ProductType == 2)
-            {
-                return (Price / 100) * 10;
-            }
-            if (ProductType == 3)
-            {
-                return (Price / 100) * 15;
-            }
-
             return 0;
         }
+
+        //public double GetDiscount()
+        //{
+        //    if (ProductType == 1)
+        //    {
+        //        return (Price / 100) * 5;
+        //    }
+
+        //    if (ProductType == 2)
+        //    {
+        //        return (Price / 100) * 10;
+        //    }
+        //    if (ProductType == 3)
+        //    {
+        //        return (Price / 100) * 15;
+        //    }
+
+        //    return 0;
+        //}
     }
 }
